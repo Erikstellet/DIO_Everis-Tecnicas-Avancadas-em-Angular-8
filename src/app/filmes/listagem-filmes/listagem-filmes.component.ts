6de3b85f9ae4c745c835066e369d74899a39b0fa
Erikstellet <erikstellet@gmail.com>
2020-12-16 16:28:37 -0300

    Listagem de Resultados e Melhoria de Performance Parte 2

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { FilmesService } from 'src/app/core/filmes.service';
 import { Filme } from 'src/app/shared/models/filme';
 
-@Component({
+@Component
+({
   selector: 'dio-listagem-filmes',
   templateUrl: './listagem-filmes.component.html',
   styleUrls: ['./listagem-filmes.component.scss']
@@ -11,16 +12,45 @@ import { Filme } from 'src/app/shared/models/filme';
 export class ListagemFilmesComponent implements OnInit {
 
   readonly qtdPagina = 5;
+
+  texto: string;
+  genero: string;
   pagina = 0;
+
   filmes: Filme[] = [];
+  filtrosListagem: FormGroup;
+  generos:  Array<string>;
 
   constructor
   (
-    private filmesService: FilmesService
+    private filmesService: FilmesService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void
   {
+    this.filtrosListagem = this.fb.group
+    (
+      {
+        texto: [''],
+        genero: ['']
+      }
+    );
+
+    this.filtrosListagem.get('texto').valueChanges.subscribe((val: string) =>
+    {
+      this.texto = val;
+      this.resetarConsulta()
+    })
+
+    this.filtrosListagem.get('genero').valueChanges.subscribe((val: string) =>
+    {
+      this.genero = val;
+      this.resetarConsulta()
+    })
+
+    this.generos = [ 'Ação', 'Aventura', 'Comédia', 'Ficção Científica', 'Romance', 'Terror' ];
+
     this.listarFilmes();
   }
 
@@ -32,7 +62,15 @@ export class ListagemFilmesComponent implements OnInit {
   private listarFilmes(): void
   {
     this.pagina++;
-    this.filmesService.listar(this.pagina, this.qtdPagina)
+    this.filmesService.listar(this.pagina, this.qtdPagina, this.texto, this.genero)
                       .subscribe((filmes: Filme[]) => this.filmes.push(...filmes));
+  }
+
+  private resetarConsulta(): void
+  {
+    this.pagina = 0;
+    this.filmes = [];
+
+    this.listarFilmes();
   }
 }
