539bedc3e71d449f6b8d0efa8c996117251d62dc
Erikstellet <erikstellet@gmail.com>
2020-12-17 16:03:01 -0300

    Finalizando o Projeto Prático Parte 1

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -2,39 +2,41 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Filme } from '../shared/models/filme';
+import { ConfigParams } from '../shared/models/config-params';
+import { ConfigParamsService } from './config-params.service';
 
 const url = 'http://localhost:3000/filmes/';
 
-@Injectable({
+@Injectable
+({
   providedIn: 'root'
 })
 export class FilmesService {
 
-  constructor(private http: HttpClient) { }
+  constructor
+  (
+    private http: HttpClient,
+    private configService: ConfigParamsService
+  ) { }
 
   salvar(filme: Filme): Observable<Filme> {
     return this.http.post<Filme>(url, filme);
   }
 
-  listar(pagina: number, qtdPagina: number, texto: string, genero: string): Observable<Filme[]>
+  listar(config: ConfigParams): Observable<Filme[]>
   {
-    let httpParams = new HttpParams();
+    const configParams = this.configService.configurarParametros(config);
 
-    httpParams = httpParams.set('_page', pagina.toString());
-    httpParams = httpParams.set('_limit', qtdPagina.toString());
-    httpParams = httpParams.set('_sort', 'dtLancamento');
-    httpParams = httpParams.set('_order', 'desc');
+    return this.http.get<Filme[]>(url, {params: configParams});
+  }
 
-    if(texto)
-    {
-      httpParams = httpParams.set('q', texto);
-    }
+  visualizar(id: number): Observable<Filme>
+  {
+    return this.http.get<Filme>(url + id);
+  }
 
-    if(genero)
-    {
-      httpParams = httpParams.set('genero', genero);
-    }
-
-    return this.http.get<Filme[]>(url, {params: httpParams});
+  excluir(id: number): Observable<void>
+  {
+    return this.http.delete<void>(url + id);
   }
 }
---
+++ src/app/filmes/visualizar-filmes/visualizar-filme.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FilmesService } from 'src/app/core/filmes.service';
+import { AlertaComponent } from 'src/app/shared/components/alerta/alerta.component';
+import { Alerta } from 'src/app/shared/models/alerta';
+import { Filme } from 'src/app/shared/models/filme';
+
+@Component
+({
+  selector: 'dio-visualizar-filme',
+  templateUrl: './visualizar-filme.component.html',
+  styleUrls: ['./visualizar-filme.component.scss']
+})
+export class VisualizarFilmesComponent implements OnInit
+{
+  readonly semFoto = "https://cutt.ly/KhKtsdK";
+
+  filme: Filme;
+  id: number;
+
+  constructor
+  (
+    public dialog: MatDialog,
+    private activatedRoute: ActivatedRoute,
+    private filmesService: FilmesService,
+    private router: Router
+  ) { }
+
+  ngOnInit()
+  {
+    this.id = this.activatedRoute.snapshot.params['id'];
+    this.visualizar();
+  }
+
+  private excluir(): void
+  {
+    const config =
+      {
+        data:
+          {
+            titulo: "Você realmente que apagar?",
+            descricao: "Caso tenha certeza, clique no botão Ok",
+            corBtnSucesso: 'warn',
+            corBtnCancelar: 'primary',
+            possuirBtnFechar: true
+          } as Alerta
+      }
+
+      const dialogRef = this.dialog.open(AlertaComponent, config);
+
+      dialogRef.afterClosed().subscribe((opcao: boolean) =>
+      {
+        if(opcao)
+        {
+          this.filmesService.excluir(this.id)
+                            .subscribe(() => this.router.navigateByUrl('/filmes'));
+        }
+      });
+  }
+
+  private visualizar(): void
+  {
+    this.filmesService.visualizar(this.id)
+                      .subscribe((filme: Filme) => this.filme = filme);
+  }
+}
